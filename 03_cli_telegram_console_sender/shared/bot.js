3e9257e9178854314b3f4f65d12f57635ca081d7
qniso <gnom51764@gmail.com>
2023-10-25 17:49:14 +0300

    [FIX] Removed console.logs

--- 03_cli_telegram_console_sender/shared/bot.js
+++ 03_cli_telegram_console_sender/shared/bot.js
@@ -3,14 +3,11 @@ import TelegramBot from "node-telegram-bot-api";
 const token = "";// Input tour bot token
 const chatId = 0; // Input tour chatID
 
-console.log(token, chatId);
-process.exit(1);
 export function telegramBot(message, method) {
-    console.log(token);
 
     const bot = new TelegramBot(token);
 
-    if (method == "m") {
+    if (method == "m") { 
         bot.sendMessage(chatId, message);
     }
     if (method == "p") {
